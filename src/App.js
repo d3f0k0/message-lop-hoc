02b530263fee6f6e4cc1553adb6c798ab5deef28
unknown <hellomeandyou09@gmail.com>
2022-10-27 15:30:36 +0700

    omit config , too risky

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ firebase.initializeApp({
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
-  measurementId: process.env.MESSAGING_SENDER_ID
+  measurementId: process.env.MEASUREMENT_ID
 })
 
 const auth = firebase.auth()
